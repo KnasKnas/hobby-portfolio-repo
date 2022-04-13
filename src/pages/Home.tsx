9f45caa68da7121e07d9f03219dd0bec06791e22
KnasKnas <christian.rudfoss@gmail.com>
2022-04-13 16:22:54 +0200

    More base setups

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Home = (): JSX.Element => {
+    return (
+        <>
+            eyy
+        </>
+    )
+
+}
+
+export default Home
---
+++ src/utils/firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app"
+// import { getAnalytics } from "firebase/analytics"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyB5UV4aOG6069Up4FoEXfM08vUeEd2okw8",
+	authDomain: "hobby-portfolio.firebaseapp.com",
+	projectId: "hobby-portfolio",
+	storageBucket: "hobby-portfolio.appspot.com",
+	messagingSenderId: "535872294570",
+	appId: "1:535872294570:web:ed0b7f88a92233f5f28ad3",
+	measurementId: "G-RZ9XZ6N7L7",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+// const analytics = getAnalytics(app)
